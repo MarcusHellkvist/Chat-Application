d9d728f9e658fe2c6c0a7dc32fd4711d4e5e7742
atlalbasha <atlal.basha@gmail.com>
2020-10-29 11:59:40 +0100

    create a new sing up user to firebase

--- app/src/main/java/com/example/chatapplication/MainActivity.java
+++ app/src/main/java/com/example/chatapplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.chatapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void singUpButtonPressed(View view) {
+        singUPIntentActivity();
+    }
+
+    public void loginButtonPressed(View view) {
+    }
+
+    public void singUPIntentActivity(){
+        Intent intent = new Intent(this,SingUp.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/chatapplication/SingUp.java
@@ -0,0 +1,52 @@
+package com.example.chatapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SingUp extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    private EditText email;
+    private EditText pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+
+        email = findViewById(R.id.user_email_editText);
+        pass = findViewById(R.id.user_pass_editText);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    public void singUpButtonPressed(View view) {
+        String userEmail = email.getText().toString();
+        String userPass = pass.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(userEmail, userPass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Toast.makeText(SingUp.this,"user done log in ",Toast.LENGTH_SHORT).show();
+                }else {
+
+                    Toast.makeText(SingUp.this,task.getException().getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
