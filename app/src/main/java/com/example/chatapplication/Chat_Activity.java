bba6f603a68f403c2af042e81fdbc1b124bf9459
alonakirichenkoivanovna <alena1412199217@gmail.com>
2020-11-02 16:02:13 +0100

    Merge branch 'main' of /Users/alonakirichenkoivanovna/AndroidStudioProjects/Chat-Application with conflicts.

---
+++ app/src/main/java/com/example/chatapplication/Chat_Activity.java
@@ -0,0 +1,13 @@
+package com.example.chatapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Chat_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_);
+    }
+}
