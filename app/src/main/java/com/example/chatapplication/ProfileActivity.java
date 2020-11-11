261a2664157498f5ef7301afd2fe84bebe20ec3a
Maco <hellqvistmarcus@gmail.com>
2020-11-11 15:26:08 +0100

    Can change profile attributes, added a dialog pop up, and a realtime snapshotlistener

--- app/src/main/java/com/example/chatapplication/ProfileActivity.java
+++ app/src/main/java/com/example/chatapplication/ProfileActivity.java
@@ -1,9 +1,13 @@
 package com.example.chatapplication;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,17 +15,28 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements ProfileDialog.ProfileDialogListener {
+
+    private static final int REQUEST_CODE_CAMERA = 1;
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
+    private FirebaseStorage storage;
     private CollectionReference usersRef;
 
     private ImageView ivProfilePicture;
@@ -29,6 +44,7 @@ public class ProfileActivity extends AppCompatActivity {
     private Button btnEditProfile;
 
     private User user;
+    private String currentUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +54,9 @@ public class ProfileActivity extends AppCompatActivity {
         // FIREBASE
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance();
         usersRef = db.collection("users");
+        currentUserId = mAuth.getCurrentUser().getUid();
 
         // IMAGE VIEWS
         ivProfilePicture = findViewById(R.id.iv_profile_picture);
@@ -59,9 +77,27 @@ public class ProfileActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        usersRef.document(currentUserId)
+                .addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if (value.exists()){
+                            String name = value.getString("name");
+                            String phone = value.getString("phoneNumber");
+                            tvUsername.setText(name);
+                            tvPhone.setText(phone);
+                        }
+                    }
+                });
+    }
+
+
     private void setUserData() {
         String idNumber = "#3391";
-        String username = user.getName().toString();
+        String username = user.getName();
         String email = user.getEmail();
         String phone = user.getPhoneNumber();
         String address = "SKÖVDE, SWEDEN";
@@ -74,6 +110,12 @@ public class ProfileActivity extends AppCompatActivity {
         tvAddress.setText(address);
         tvAmountFriends.setText(amountOfFriends);
     }
+
+    private void openDialog(){
+        ProfileDialog profileDialog = new ProfileDialog(user);
+        profileDialog.show(getSupportFragmentManager(), "profile dialog");
+    }
+
 
     private void getUserData() {
         String currentUserId = mAuth.getCurrentUser().getUid();
@@ -94,16 +136,47 @@ public class ProfileActivity extends AppCompatActivity {
                         } else {
                             Log.d("TAG", "onComplete: get failed with ", task.getException());
                         }
-
                     }
                 });
     }
 
-
     private View.OnClickListener editListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Toast.makeText(ProfileActivity.this, "edit.", Toast.LENGTH_SHORT).show();
+            openDialog();
         }
     };
+
+    @Override
+    public void updateUserInfo(String name, String phone) {
+
+        // Update User class.
+        user.setName(name);
+        user.setPhoneNumber(phone);
+
+        FirebaseUser fUser = mAuth.getCurrentUser();
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(name)
+                .build();
+
+        // update auth
+        fUser.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            // update firestore
+                            usersRef.document(currentUserId)
+                                    .set(user)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Toast.makeText(ProfileActivity.this, "profile updated.", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                        }
+                    }
+                });
+
+    }
 }
---
+++ app/src/main/java/com/example/chatapplication/ProfileDialog.java
@@ -0,0 +1,72 @@
+package com.example.chatapplication;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class ProfileDialog extends AppCompatDialogFragment {
+
+    private EditText etDialogProfileName, etDialogProfilePhone;
+    private ProfileDialogListener listener;
+    private User user;
+
+    public ProfileDialog(User user){
+        this.user = user;
+    }
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View v = inflater.inflate(R.layout.profile_dialog, null);
+
+        builder.setView(v)
+                .setTitle("Update profile")
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setPositiveButton("Update", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String name = etDialogProfileName.getText().toString();
+                        String phone = etDialogProfilePhone.getText().toString();
+                        listener.updateUserInfo(name, phone);
+                    }
+                });
+
+        etDialogProfileName = v.findViewById(R.id.et_dialog_profile_name);
+        etDialogProfilePhone = v.findViewById(R.id.et_dialog_profile_phone);
+        etDialogProfileName.setText(user.getName());
+        etDialogProfilePhone.setText(user.getPhoneNumber());
+        return builder.create();
+
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        try {
+            listener = (ProfileDialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement ProfileDialogListener");
+        }
+    }
+
+    public interface ProfileDialogListener {
+        void updateUserInfo(String name, String phone);
+    }
+}
