7bd0a2871db7ffa223ce0826518cf182508517e4
jenny <jenny.rydebrink@gardenize.se>
2020-11-16 15:44:28 +0100

    Implemented scroll view on log in and sign up page. Everything works :-) /JR

--- app/src/main/java/com/example/chatapplication/SingUp.java
+++ app/src/main/java/com/example/chatapplication/SingUp.java
@@ -42,7 +42,7 @@ public class    SingUp extends AppCompatActivity {
         email = findViewById(R.id.user_email_editText);
         pass = findViewById(R.id.user_pass_editText);
         confrimPassword = findViewById(R.id.user_confirm_pass_editText);
-        name = findViewById(R.id.search_user_name);
+        name = findViewById(R.id.user_name);
         //userNumber = findViewById(R.id.user_telNumber);
         db = FirebaseFirestore.getInstance();
 
@@ -53,71 +53,48 @@ public class    SingUp extends AppCompatActivity {
     }
 
     public void singUpButtonPressed(View view) {
-        final String userEmail = email.getText().toString().trim();
 
-        String userPass = pass.getText().toString().trim();
-        final String userName = name.getText().toString().trim();
-        final String userTelNumber = userNumber.getText().toString().trim();
+                    final String userEmail = email.getText().toString().toLowerCase().trim();
+                    final String userPass = pass.getText().toString().trim();
+                    final String confirmedPass = confrimPassword.getText().toString().trim();
+                    final String userName = name.getText().toString().trim().toLowerCase();
+                    //final String userTelNumber = userNumber.getText().toString().trim();
 
-        mAuth.createUserWithEmailAndPassword(userEmail, userPass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()){
-                    //get user id from Firebase
-                    idFirebase = mAuth.getCurrentUser().getUid();
-                    User user = new User(userName,userTelNumber,userEmail, idFirebase );
-                    db.collection("users").document(idFirebase).set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            Intent intent = new Intent(SingUp.this, Chat_Activity.class);
-                            startActivity(intent);
-                        }
-                    });
-                   // FirebaseUser user = mAuth.getCurrentUser();
-                    Toast.makeText(SingUp.this,"user done log in ",Toast.LENGTH_SHORT).show();
-                }else {
-
-                    Toast.makeText(SingUp.this,task.getException().getLocalizedMessage(),Toast.LENGTH_SHORT).show();
-
-        final String userPass = pass.getText().toString().trim();
-        final String confirmedPass = confrimPassword.getText().toString().trim();
-        final String userName = name.getText().toString().trim().toLowerCase();
-        //final String userTelNumber = userNumber.getText().toString().trim();
-
-        //Added an if statement to make sure the password is entered correctly. /JR
-        if (userPass.equals(confirmedPass)) {
-            mAuth.createUserWithEmailAndPassword(userEmail, userPass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-
-                    if (task.isSuccessful()) {
-                        //get user id from Firebase
-                        idFirebase = mAuth.getCurrentUser().getUid();
-                        User user = new User(userName, userEmail, idFirebase);
-                        db.collection("users").document(idFirebase).set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    //Added an if statement to make sure the password is entered correctly. /JR
+                    if (userPass.equals(confirmedPass)) {
+                        mAuth.createUserWithEmailAndPassword(userEmail, userPass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                             @Override
-                            public void onSuccess(Void aVoid) {
-                                Intent intent = new Intent(SingUp.this, Chat_Activity.class);
-                                startActivity(intent);
-                                Log.d("Jenny", "Ny använade skall vara skapad");
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()) {
+                                    //get user id from Firebase
+                                    idFirebase = mAuth.getCurrentUser().getUid();
+                                    User user = new User(userName, userEmail, idFirebase);
+                                    db.collection("users").document(idFirebase).set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Intent intent = new Intent(SingUp.this, Chat_Activity.class);
+                                            startActivity(intent);
+                                            Log.d("Jenny", "Ny använade skall vara skapad");
+                                        }
+                                    });
+                                    // FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(SingUp.this, "user done log in ", Toast.LENGTH_SHORT).show();
+
+                                } else {
+
+                                    Toast.makeText(SingUp.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                                }
+
+
                             }
+
+
                         });
-                        // FirebaseUser user = mAuth.getCurrentUser();
-                        Toast.makeText(SingUp.this, "user done log in ", Toast.LENGTH_SHORT).show();
-
                     } else {
-
-                        Toast.makeText(SingUp.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SingUp.this, "Passwords don't match, pls try again", Toast.LENGTH_LONG).show();
                     }
-
-
                 }
 
-
-            });
-        } else {
-            Toast.makeText(SingUp.this, "Passwords don't match, pls try again", Toast.LENGTH_LONG).show();
-        }
-    }
 
 }
